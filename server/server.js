d8e3a529ce44cca5205e86107dc69589d3fb39f5
YanisGh <yanisghoul35@gmail.com>
2023-06-25 16:07:37 +0200

    retrieve des images captcha + add img in folder OK

--- server/server.js
+++ server/server.js
@@ -4,9 +4,32 @@ const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const crypto = require('crypto');
 const cors = require('cors'); // Added
-
+const fs = require('fs');
 const app = express();
-app.use(express.static('public'));
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+const multer = require('multer');
+const AdmZip = require('adm-zip');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (file.fieldname === 'neutralPics') {
+      cb(null, 'C:/Users/yanis/CAPchat/Cap-Chat/public/captchaIMG/neutres/');
+    } else if (file.fieldname === 'singularPic') {
+      cb(null, 'C:/Users/yanis/CAPchat/Cap-Chat/public/captchaIMG/singuliers/');
+    } else {
+      cb(new Error('Invalid file fieldname'));
+    }
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+//app.use(express.static('public'));
 app.use(cors()); // Added
 
 function MD5(string) {
@@ -69,8 +92,7 @@ app.post('/register', (req, res) => {
 // Route d'authentification
 app.get('/login', (req, res) => {
   const username = req.query.username;
-  const password = req.query.password;
-  const hashedPassword = MD5(password);
+  const hashedPassword = MD5(req.query.password);
 
   const query = `SELECT * FROM utilisateurs WHERE identifiant = ? AND MotDePasse = ?`;
   connection.query(query, [username, hashedPassword], (err, results) => {
@@ -83,7 +105,7 @@ app.get('/login', (req, res) => {
     // Check if the login credentials are valid
     if (results.length > 0) {
       const token = jwt.sign({ username }, 'audir8');
-      const userId = results[0].idUtilisateur; // Assuming the user ID field is named 'id'
+      const userId = results[0].idUtilisateur;
       
       // Update the token field in the database for the logged-in user
       const updateQuery = 'UPDATE utilisateurs SET token = ? WHERE idUtilisateur = ?';
@@ -114,13 +136,13 @@ app.get('/getAll', (req, res) => {
 
   switch (categorie) {
     case 'theme':
-      query = 'SELECT idTheme, Nom FROM themes';
+      query = 'SELECT idTheme, Nom FROM themes ORDER BY Nom';
       break;
     case 'artistes':
-      query = 'SELECT idUtilisateur, Identifiant FROM Utilisateurs';
+      query = 'SELECT idUtilisateur, Identifiant FROM Utilisateurs ORDER BY Identifiant';
       break;
     case 'jeux d\'image':
-      query = 'SELECT idJeu, Nom FROM jeux';
+      query = 'SELECT idJeu, Nom FROM jeux ORDER BY Nom';
       break;
     default:
       res.status(400).send('Invalid categorie');
@@ -138,10 +160,72 @@ app.get('/getAll', (req, res) => {
 });
 
 
-//Route pour crée les themes, artistes, jeux d'images.
-app.post('/postAll', (req, res) => {
+//Route pour crée les jeux.
+app.post('/postForm', upload.fields([
+  { name: 'neutralPics', maxCount: 7 },
+  { name: 'singularPic', maxCount: 1 }
+]), (req, res) => {
 
+  const username = req.body.username;
+  const captchatName = req.body.captchatName;
+  const themeId = req.body.themeId;
+  const themeName = req.body.themeName;
+  const singularPicHint = req.body.singularPicHint;
+
+  console.log('Username:', username);
+  console.log('CaptChat Name:', captchatName);
+  console.log('Theme ID:', themeId);
+  console.log('Theme Name:', themeName);
+  console.log('Singular Pic Hint:', singularPicHint);
+
+  const neutralPic = req.files['neutralPics'][0];
+  const singularPic = req.files['singularPic'][0];
+
+  const neutralZip = new AdmZip(neutralPic.path);
+  const neutralEntries = neutralZip.getEntries();
+
+  neutralEntries.forEach((entry) => {
+    //console.log(entry);
+    if (!entry.isDirectory) {
+      const extractedPath = path.join('C:/Users/yanis/CAPchat/Cap-Chat/public/captchaIMG/neutres', entry.entryName);
+      const entryData = entry.getData();
+      fs.writeFileSync(extractedPath, entryData);
+      console.log('Neutral Picture:', entry.entryName);
+    }
+  });
+  
+  const singularZip = new AdmZip(singularPic.path);
+  const singularEntries = singularZip.getEntries();
+
+  singularEntries.forEach((entry) => {
+    //console.log(entry);
+    if (!entry.isDirectory) {
+      const extractedPath = 'C:/Users/yanis/CAPchat/Cap-Chat/public/captchaIMG/singuliers/' + entry.entryName;
+      const entryData = entry.getData();
+      fs.writeFileSync(extractedPath, entryData);
+      console.log('Singular Picture:', entry.entryName);
+    }
+  });
+
+
+  // fs.unlinkSync(neutralPic.path);
+  // fs.unlinkSync(singularPic.path);
+
+  const insertCaptchatQuery = `INSERT INTO captchat (Nom, idTheme, idUtilisateur) VALUES ('${captchatName}', ${themeId}, '25') `;
+  connection.query(insertCaptchatQuery, (error, captchatResult) => {
+    if (error) {
+      console.error('Error inserting captchat:', error);
+      // Handle the error
+    } else {
+      // Captchat and theme inserted successfully
+      console.log('Captchat and theme inserted successfully');
+      // Handle the success
+    }
+
+  res.sendStatus(200);
+  });
 });
+
 
 app.patch('/modifyAll', (req, res) => {
   const { categorie, id, newName } = req.body;
@@ -206,6 +290,61 @@ app.delete('/deleteAll', (req, res) => {
     res.send(categorie + 'deleted successfully');
   });
 });
+
+
+app.get('/captcha/:idJeu', (req, res) => {
+ 
+  const idJeu = req.params.idJeu;
+  // Query to retrieve the singular image with a question associée
+  const singularQuery = `SELECT NomIMG, QuestionAssociee FROM images WHERE idJeu = ${idJeu} AND typeIMG = 1 LIMIT 1`;
+
+  // Query to retrieve the neutral images
+  const neutralQuery = `SELECT NomIMG FROM images WHERE idJeu = ${idJeu} AND typeIMG = 0`;
+
+  // Execute the query to retrieve the images with the given idJeu
+  connection.query(singularQuery, (err, singularResults) => {
+    if (err) {
+      console.error('Error executing the singular image query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    connection.query(neutralQuery, (err, neutralResults) => {
+      if (err) {
+        console.error('Error executing the neutral images query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+
+    // Extract the image names and questions from the query results
+    const neutralImages = neutralResults.map((row) => row.NomIMG);
+    const singularImage = singularResults[0] || null;
+    const singularImageName = singularImage ? singularImage.NomIMG : null;
+    const singularImageQuestion = singularImage ? singularImage.QuestionAssociee : null;
+    
+    // console.log("neutral image 1: " + neutralImages);
+    // console.log("singularImage name: " + singularImageName);
+    // console.log("singularImage question: " + singularImageQuestion);
+    
+    const captchaData = {
+      neutralImages: neutralImages,
+      singularImage: {
+        NomIMG: singularImageName,
+        QuestionsAssociee: singularImageQuestion,
+      },
+    };
+    
+
+      console.log("neutral image 2 : " + neutralImages)
+      console.log("singularImage 2 : " + singularImage.NomIMG + " question : " + singularImageQuestion)
+
+      // Render the captcha view and pass the captcha data to the template
+      res.json({ captchaData });
+    });
+});
+});
+
+
 
 
 // Middleware pour vérifier le token d'authentification
